044d54f445fcf28cb329d795e5d78a99e470f9fe
adakayaaa <ummuhaneltn@gmail.com>
2023-09-13 08:17:11 +0200

    changed assignnment 29 week 5

--- src/week_05/assignments/Question_05_29.java
+++ src/week_05/assignments/Question_05_29.java
@@ -3,7 +3,7 @@ package week_05.assignments;
 import java.util.Scanner;
 
 public class Question_05_29 {
-    public static void main (String[] args){
+    public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter year (e.g.2012) : ");
         int year = input.nextInt();
@@ -16,13 +16,13 @@ public class Question_05_29 {
 
             switch (month) {
                 case 1:
-                    numberOfDaysInMonth = 31;
                     System.out.print("January");
+                    numberOfDaysInMonth = 31;
                     break;
                 case 2:
-
                     System.out.print("February");
-                    day += 3;
+                    numberOfDaysInMonth = ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) ? 29 : 28;
+                    day += 31;
                     break;
                 case 3:
                     numberOfDaysInMonth = 31;
@@ -32,62 +32,63 @@ public class Question_05_29 {
                 case 4:
                     numberOfDaysInMonth = 30;
                     System.out.print("April");
-                    day += 3;
+                    day += 31;
                     break;
                 case 5:
                     numberOfDaysInMonth = 31;
                     System.out.print("May");
-                    day += 2;
+                    day += 30;
                     break;
                 case 6:
                     numberOfDaysInMonth = 30;
                     System.out.print("June");
-                    day += 3;
+                    day += 31;
                     break;
                 case 7:
                     numberOfDaysInMonth = 31;
                     System.out.print("July");
-                    day += 2;
+                    day += 30;
                     break;
                 case 8:
                     numberOfDaysInMonth = 31;
                     System.out.print("August");
-                    day += 3;
+                    day += 31;
                     break;
                 case 9:
                     numberOfDaysInMonth = 30;
                     System.out.print("September ");
-                    day += 3;
+                    day += 31;
                     break;
                 case 10:
                     numberOfDaysInMonth = 31;
                     System.out.print("October");
-                    day += 2;
+                    day += 30;
                     break;
                 case 11:
                     numberOfDaysInMonth = 30;
-                    System.out.print("November ");
-                    day += 3;
+                    System.out.print("November");
+                    day += 31;
                     break;
                 case 12:
                     numberOfDaysInMonth = 31;
                     System.out.print("December");
-                    day += 2;
+                    day += 31;
             }
             System.out.println("-" + year);
-            System.out.println("-------------------------------------");
-            System.out.println("Sun Mon Tue Wed Thu Fri Sat Sun\n");
+            System.out.println("-----------------------------------");
+            System.out.println("Sun  Mon  Tue  Wed  Thu  Fri  Sat");
 
-            for (int j =1; j <=day %7 ;j++){
-                System.out.println("    ");
+            for (int j = 0; j < day % 7; j++) {
+                System.out.printf("%5s", " ");
             }
-            for (int j= 1; j <= numberOfDaysInMonth ; j++ ){
-                if ((day +j) %7 == 0){
-                    System.out.printf("%4d\n",j);
-                }else{
-                    System.out.printf("%4d" ,j);
+            for (int j = 1; j <= numberOfDaysInMonth; j++) {
+                if ((day + j) % 7 == 0) {
+                    System.out.printf("%-5d\n", j);
+                } else {
+                    System.out.printf("%-5d", j);
                 }
             }
+            System.out.println("\n");
 
         }
 
