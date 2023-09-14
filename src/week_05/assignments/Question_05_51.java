36b72724763a280b4a6ad0cba1acff2fb97584e4
adakayaaa <ummuhaneltn@gmail.com>
2023-09-14 22:08:48 +0200

    added last assignment

--- src/week_05/assignments/Question_05_51.java
+++ src/week_05/assignments/Question_05_51.java
@@ -3,10 +3,33 @@ package week_05.assignments;
 import java.util.Scanner;
 
 public class Question_05_51 {
-    public static void main (String[] args){
+    public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        System.out.println("Enter the first string: ");
+        String firstString = input.nextLine();
 
+        System.out.println("Enter the second string: ");
+        String secondString = input.nextLine();
+        String theLargestCommonPrefix = "";
 
+        //Lowercase letters and upper case letters are distinguishing feature at this solution.
+        int minLength = Math.min(firstString.length(), secondString.length());
 
+        for (int i = 0; i <= minLength; i++) {
+            char firstStringCharacters = firstString.charAt(i);
+            char secondStringCharacters = secondString.charAt(i);
+
+            if (firstStringCharacters == secondStringCharacters) {
+                theLargestCommonPrefix += firstString.charAt(i);
+            }else{
+                break;
+            }
+        }
+
+        if(theLargestCommonPrefix.length()!= 0) {
+            System.out.println("The common prefix is " + theLargestCommonPrefix);
+        }else{
+            System.out.println(firstString + " and " + secondString + " have no common prefix");
+        }
     }
 }
