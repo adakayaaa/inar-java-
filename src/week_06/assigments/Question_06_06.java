48a5cd6c7c31627ea04dd25defb198999f9c27a0
adakayaaa <ummuhaneltn@gmail.com>
2023-09-18 21:13:51 +0200

    added assignment6 week 6

--- src/week_06/assigments/Question_06_06.java
+++ src/week_06/assigments/Question_06_06.java
@@ -1,4 +1,23 @@
 package week_06.assigments;
 
+import java.util.Scanner;
+
 public class Question_06_06 {
+    public static void main (String[]args){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the number of rows: ");
+        int numberOfRows = input.nextInt();
+        displayPattern(numberOfRows);
+    }
+    public static void displayPattern( int n){
+        for( int i =1; i<=n ; i++){
+            for (int j =1; j<=7-i ; j++){
+                System.out.print ("  ");
+            }
+            for (int k = i; k >= 1 ; k--){
+                System.out.print(k + " ");
+            }
+            System.out.println();
+        }
+    }
 }
