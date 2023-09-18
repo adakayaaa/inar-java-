f18c4c906e7a6ca73587def3905cf55c120c1d96
adakayaaa <ummuhaneltn@gmail.com>
2023-09-18 20:46:15 +0200

    added new assignments week 6

--- src/week_06/assigments/Question_06_03.java
+++ src/week_06/assigments/Question_06_03.java
@@ -1,7 +1,36 @@
 package week_06.assigments;
 
+import java.util.Scanner;
+
 public class Question_06_03 {
-    public static void main (String[] args){
-        
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter an integer: ");
+        int number = input.nextInt();
+        reverse(number);
+    }
+
+    public static int reverse(int number) {
+        if (isPalindrome(number)) {
+            System.out.print(number + " is a palindrome");
+        } else {
+            System.out.print(number + " is not a palindrome");
+        }
+        return number;
+    }
+
+    public static boolean isPalindrome(int number) {
+        String numberNew = number + "";
+        String output = "";
+        for (int i = numberNew.length()-1; i >= 0; i--) {
+            output += numberNew.charAt(i);
+        }
+        if (numberNew.compareTo(output) !=0) {
+                return false;
+        }
+        return true;
     }
 }
+
+
+
---
+++ src/week_06/assigments/Question_06_05.java
@@ -0,0 +1,36 @@
+package week_06.assigments;
+
+import java.util.Scanner;
+
+public class Question_06_05 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter three numbers: ");
+        double number1 = input.nextDouble();
+        double number2 = input.nextDouble();
+        double number3 = input.nextDouble();
+        displaySortedNumbers(number1, number2, number3);
+    }
+
+    public static void displaySortedNumbers(double num1, double num2, double num3) {
+
+        double tempMax = Math.max(num1, num2);
+        double max = Math.max(tempMax, num3);
+
+        double tempMin = Math.min(num1, num2);
+        double min = Math.min(tempMin, num3);
+
+        double mid ;
+        if (num1 != max && num1 != min) {
+            mid = num1;
+
+        } else if (num2 != max && num2 != min) {
+            mid = num2;
+
+        } else {
+            mid = num3;
+        }
+
+        System.out.println(min +" " +  mid  +" " + max);
+    }
+}
