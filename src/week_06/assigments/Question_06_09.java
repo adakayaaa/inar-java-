804482ecad55a34ea92a2c4295ebf9ec0fb4bfac
adakayaaa <ummuhaneltn@gmail.com>
2023-09-19 18:05:06 +0200

    added new assignments

---
+++ src/week_06/assigments/Question_06_09.java
@@ -0,0 +1,34 @@
+package week_06.assigments;
+
+public class Question_06_09 {
+    public static void main(String[] args) {
+        System.out.println("Feet" + "       " + "Meters" + "       |" +
+                "       " + "Meters" + "     " + "Feet");
+        System.out.println("--------------------------------------------------------------");
+
+        double feet = 1;
+        double meter = 20;
+        while (feet<= 10 && meter <= 65) {
+            System.out.printf("%-10.1f %-10.3f   |       %-10.1f%-10.3f"
+                    , feet,
+                   footToMeter(feet),
+                    meter,
+                    meterToFoot(meter));
+           feet ++;
+            meter+= 5;
+            System.out.println();
+        }
+
+    }
+
+    public static double footToMeter(double foot) {
+        double meter = 0.305 * foot;
+        return meter;
+    }
+
+    public static double meterToFoot(double meter) {
+        double foot = 3.279 * meter;
+        return foot;
+    }
+}
+
