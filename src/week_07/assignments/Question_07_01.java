f2453e56cc9808b9e5525f65541a6a682df1adbe
adakayaaa <ummuhaneltn@gmail.com>
2023-10-02 22:26:18 +0200

    added assignments-3 week-7

--- src/week_07/assignments/Question_07_01.java
+++ src/week_07/assignments/Question_07_01.java
@@ -1,2 +1,36 @@
-package week_07.assignments;public class Question_07_01 {
+package week_07.assignments;
+
+import java.util.Scanner;
+
+public class Question_07_01 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the number of students: ");
+        int number = input.nextInt();
+        int[] numberOfStudents = new int[number];
+
+        System.out.println("Enter " + number + " scores: ");
+        int max = numberOfStudents[0];
+        for (int i = 0; i < numberOfStudents.length; i++) {
+            numberOfStudents[i] = input.nextInt();
+            if (numberOfStudents[i] > max) {
+                max = numberOfStudents[i];
+            }
+        }
+
+        for (int i = 0; i < numberOfStudents.length; i++) {
+            if (numberOfStudents[i] >= max - 10) {
+                System.out.println("Student " + i + "score is " + numberOfStudents[i] + " and the grade is A");
+            } else if (numberOfStudents[i] >= max - 20) {
+                System.out.println("Student " + i + "score is " + numberOfStudents[i] + " and the grade is B");
+            } else if (numberOfStudents[i] >= max - 30) {
+                System.out.println("Student " + i + "score is " + numberOfStudents[i] + " and the grade is C");
+            } else if (numberOfStudents[i] >= max - 40) {
+                System.out.println("Student " + i + "score is " + numberOfStudents[i] + " and the grade is D");
+            } else {
+                System.out.println("Student " + i + "score is " + numberOfStudents[i] + " and the grade is F");
+            }
+        }
+    }
 }
+
--- src/week_07/assignments/Question_07_02.java
+++ src/week_07/assignments/Question_07_02.java
@@ -1,2 +1,21 @@
-package week_07.assignments;public class Question_07_02 {
+package week_07.assignments;
+
+import java.util.Scanner;
+
+public class Question_07_02 {
+    public static void  main (String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter ten integers: ");
+        int [] number = new int [10];
+
+        for (int i=0; i< number.length;i++){
+            number [i] = input.nextInt();
+        }
+        printReverse(number);
+    }
+    public static void printReverse(int [] number){
+        for (int i= number.length-1 ;i>=0;i--){
+            System.out.print(number[i] + " ");
+        }
+    }
 }
