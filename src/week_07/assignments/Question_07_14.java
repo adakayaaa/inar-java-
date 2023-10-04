14a15d1c8a7e68d7ed4352e26ffb316150d95910
adakayaaa <ummuhaneltn@gmail.com>
2023-10-04 21:47:38 +0200

    added assignment 14- week7

--- src/week_07/assignments/Question_07_14.java
+++ src/week_07/assignments/Question_07_14.java
@@ -8,11 +8,44 @@ public class Question_07_14 {
         int[] computingGcd = new int[5];
 
         System.out.print("Enter five numbers: ");
-
         for (int i = 0; i < computingGcd.length; i++) {
             computingGcd[i] = input.nextInt();
         }
+        System.out.println("The greatest common divisor is " + gcd(computingGcd));
+    }
+
+    public static int gcd(int... numbers) {
+        int greatCommonDivisor = 1;
+        int possibleGcd = 2;
+        int minNumber = smallestNumber(numbers);
+
+        for (possibleGcd = 2; possibleGcd <= minNumber; possibleGcd++) {
+            boolean result = true;
+            for (int i = 0; i < numbers.length; i++) {
+                if (numbers[i] % possibleGcd != 0) {
+                    result = false;
+                    break;
+                }
+            }
+            if (result) {
+                greatCommonDivisor = possibleGcd;
+            }
+
+        }
+        return greatCommonDivisor;
+    }
+
+
+    public static int smallestNumber(int[] array) {
+        int min = array[0];
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < min) {
+                min = array[i];
+            }
+        }
+        return min;
     }
 }
+
 
 
