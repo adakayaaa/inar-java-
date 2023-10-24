8bd37bf5d4feffc170aac44a14172c80e381edd1
adakayaaa <ummuhaneltn@gmail.com>
2023-10-24 21:35:08 +0200

    added new assignments

--- src/week_08/assignments/Question_08_33.java
+++ src/week_08/assignments/Question_08_33.java
@@ -3,17 +3,18 @@ package week_08.assignments;
 import java.util.Scanner;
 
 public class Question_08_33 {
-    //STH İS WRONG
     public static void main (String[] args){
         double[][] matrix = createMatrix(4, 2);
         double[] intersectingPoint = getIntersectingPoint(matrix);
-        getTriangleArea(matrix,intersectingPoint);
+        double [] areasArray = getTriangleArea(matrix,intersectingPoint);
+        sort(areasArray);
+        display(areasArray);
     }
     public static double[][] createMatrix(int row, int column) {
         Scanner input = new Scanner(System.in);
 
         double[][] matrix = new double[row][column];
-        System.out.print("Enter x1,y1,x2,y2,x3,y3,x4,y4: ");
+        System.out.print("Enter x1,y1,x3,y3,x2,y2,x4,y4: ");
         for (int i = 0; i < row; i++) {
             for (int j = 0; j < column; j++) {
                 matrix[i][j] = input.nextDouble();
@@ -35,38 +36,55 @@ public class Question_08_33 {
         solve[1]  = (a * f - e * c) / (a * d - b * c);
         return solve;
     }
-    public static void getTriangleArea (double[][] matrix, double[] array){
-        double []side1 = new double[4];
-        double []side2 = new double[4];
+    public static double[] getTriangleArea (double[][] matrix, double[] intersection){
+        double []result = new double[4];
+
+        result[0] = calculateTriangle(matrix,0,2,intersection);
+        result[1] = calculateTriangle(matrix,1,2,intersection);
+        result[2] = calculateTriangle(matrix,1,3,intersection);
+        result[3] = calculateTriangle(matrix,3,0,intersection);
+
+        return result;
+    }
+    public static double calculateTriangle(double[][] matrix,int point1,int point2, double[] intersection){
+        double side1= getDistance(matrix[point1], matrix[point2]);
+        double side2= getDistance(matrix[point1], intersection);
+        double side3= getDistance(matrix[point2], intersection);
+
+        double side = (side1 + side2 + side3) / 2;
 
 
-        for (int i = 0; i < matrix.length-1; i++) {
-            for (int j = 0; j < matrix[0].length; j++) {
-                side1[i]+= Math.pow(array[j]-matrix[i][j],2);
-                side2[i] += Math.pow(matrix[i][j]-matrix[i+1][j],2);
+        double area = Math.sqrt(side * (side - side1) * (side - side2) * (side - side3));
+        return area;
+    }
+    public static double getDistance(double[]point1,double[] point2){
+        return Math.sqrt(Math.pow(point1[0]-point2[0] ,2 ) +
+                Math.pow(point1[1]-point2[1] ,2 ));
+    }
+    public static void sort(double[] areas){
+        for (int i = 0; i < areas.length-1; i++) {
+            double min =areas[i];
+            int minIndex =i;
+            for (int j = i+1; j <areas.length; j++) {
+                if(min>areas[j]){
+                    min = areas[j];
+                    minIndex=j;
+                }
+            }
+            if(minIndex !=i){
+                double temp= areas[i];
+                areas[i] = areas[minIndex];
+                areas[minIndex]= temp;
+
             }
 
         }
-        side2[3] = Math.pow(matrix[3][0]-matrix[0][0],2)+Math.pow(matrix[3][1]-matrix[0][1],2);
-
-
-        double[]side = new double[4];
-        for (int i = 0; i < matrix.length-1; i++) {
-            side[i]= (Math.sqrt(side2[i])+Math.sqrt(side2[i+1])+Math.sqrt(side1[i]))/2;
-            }
-        side[3]=side2[3]+side2[0]+side1[3];
-
-
-
-        double [] area = new double[4];
-        for (int i = 0; i < area.length-1; i++) {
-            area[i]= Math.sqrt(side[i] * (side[i] - side2[i]) * (side[i] - side2[i+1]) * (side[i] - side1[i]));
+    }
+    public static void display(double[] areas){
+        for (int i = 0; i < areas.length; i++) {
+            System.out.printf("%-7.2f",areas[i]);
         }
-        area[3]= Math.sqrt(side[3] * (side[3] - side2[3]) * (side[3] - side2[0]) * (side[3] - side1[3]));
 
-        for (int i = 0; i < area.length; i++) {
-            System.out.println(area[i] +" ");
-        }
     }
 
 }
--- src/week_08/assignments/Question_08_35.java
+++ src/week_08/assignments/Question_08_35.java
@@ -3,10 +3,13 @@ package week_08.assignments;
 import java.util.Scanner;
 
 public class Question_08_35 {
-    //NOT FİNİSHED.
+
     public static void main(String[] args) {
         int[][] matrix = createMatrix();
         display(matrix);
+        int[] maximumSquare = getMaximumSquare(matrix);
+        displayMaximumSquare(maximumSquare);
+
 
     }
 
@@ -24,13 +27,54 @@ public class Question_08_35 {
         }
         return matrix;
     }
-    public static void display(int[][] matrix){
+
+    public static void display(int[][] matrix) {
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[0].length; j++) {
-                System.out.print( matrix[i][j]+" ");
+                System.out.print(matrix[i][j] + " ");
             }
             System.out.println();
 
         }
+    }
+
+    public static int[] getMaximumSquare(int[][] matrix) {
+        int[] result = new int[3];
+        int maxSize = 1;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                while (hasSquare(matrix, i, j, maxSize)) {
+                    result[0] = i;
+                    result[1] = j;
+                    result[2] = maxSize;
+                    maxSize++;
+                }
+            }
+
+        }
+
+        return result;
+    }
+
+    public static boolean hasSquare(int[][] matrix, int i, int j, int maxSize) {
+        if (i + maxSize > matrix.length) {
+            return false;
+        }
+        if (j + maxSize > matrix[0].length) {
+            return false;
+        }
+        for (int k = i; k < i + maxSize; k++) {
+            for (int l = j; l < j + maxSize; l++) {
+                if (matrix[k][l] != 1) {
+                    return false;
+                }
+            }
+
+        }
+        return true;
+    }
+
+    public static void displayMaximumSquare(int[] array) {
+        System.out.println("The maximum square submatrix is at (" + array[0] + " , " + array[1] + ") with size " + array[2]);
     }
 }
